7c5d7fa16297745786b98d327050dcd884825d23
simoench7 <simeonch7@gmail.com>
2018-01-11 08:05:02 +0200

    Last changes

--- spec/controllers/rsas_controller_spec.rb
+++ spec/controllers/rsas_controller_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe RsasController do 
 	it "gets by id" do
 	get :show, params: {id: 1}
-	expect(response.body).to eq "the content"
+	expect(response).to render_template :show
 	end
 end
